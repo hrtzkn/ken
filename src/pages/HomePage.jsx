c5c8755669022d3ea703277183860049bc885cea
ken <kenletigio@gmail.com>
2024-12-22 13:50:52 +0800

    Initial commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ function HomePage() {
     <section
       id="home"
       className="relative text-center mb-8 bg-cover bg-center h-[80vh] flex items-center justify-center"
-      style={{ backgroundImage: "url('/store-banner.jpg')" }}
+      style={{ backgroundImage: "url('/100.webp')" }}
     >
       <div className="bg-black bg-opacity-50 p-6 rounded-lg text-white max-w-2xl">
         <h2 className="text-4xl font-bold mb-4">Welcome to the Ultimate Online Store</h2>
